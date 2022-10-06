45e1fea5c26c09fc34768bdfdcfc2af3471221d6
Akshit Thakur <akshit2649@gmail.com>
2022-10-06 11:40:46 +0530

    morris traversal

--- Trees/morrisTraversal.cpp
+++ Trees/morrisTraversal.cpp
@@ -1,8 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int>MorrisTraversalInorder(Node* root){
+	vector<int> res;
+	Node* cur = root;
+	while(cur!=NULL){
+		if(cur->left == NULL){
+			res.push_back(cur->data);
+			cur = cur->right;
+		}
+		else{
+			Node* prev = cur->left;
+			while(prev->right && prev->right != cur){
+				prev = prev->right;
+			}
+			if(prev->right == NULL){
+				prev->right = cur;
+				cur = cur->left;
+			}
+			else{
+				//prev pointing to the cur
+				prev->right = NULL;
+				res.push_back(cur->val);
+				cur = cur->right;
+			}
+		}
+	}
+	return res;
+}
 
-
+vector<int>MorrisTraversalPreOrder(Node* root){
+	vector<int> res;
+	Node* cur = root;
+	while(cur!=NULL){
+		if(cur->left == NULL){
+			res.push_back(cur->data);
+			cur = cur->right;
+		}
+		else{
+			Node* prev = cur->left;
+			while(prev->right && prev->right != cur){
+				prev = prev->right;
+			}
+			if(prev->right == NULL){
+				prev->right = cur;
+				res.push_back(cur->val);
+				cur = cur->left;
+			}
+			else{
+				//prev pointing to the cur
+				prev->right = NULL;
+				cur = cur->right;
+			}
+		}
+	}
+	return res;
+}
 int main(){
 	return 0;
 }
